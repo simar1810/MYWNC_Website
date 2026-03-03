4954293dba9aec244e5a65fb7e7d49bfb3c93692
Simarpreet Singh <103562137+simar1810@users.noreply.github.com>
2026-03-03 12:18:24 +0530

    Update middleware.js

--- src/middleware.js
+++ src/middleware.js
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 
 const PLAY_STORE_URL =
-  'https://play.google.com/store/apps/details?id=com.wellnessz.aaharsutra';
+  'https://play.google.com/store/apps/details?id=com.wellnessz.wnc';
 const APP_STORE_URL =
   'https://apps.apple.com/in/app/healthsang/id6754602127';
 
